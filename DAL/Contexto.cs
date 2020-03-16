9b488aa19eb25b861a637ab5613068176d1e87c9
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-16 11:30:48 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial2_MiguelGondres.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlite(@"Data Source= DATA\RParcial2.db");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
