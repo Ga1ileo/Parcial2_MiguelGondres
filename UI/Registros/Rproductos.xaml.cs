018e3d233540ebe246c857d5df2e6513b4cb8536
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-16 17:22:37 -0400

    Se agrego la Carpeta UI, con sus respectivas carpetas, como las indicaciones

---
+++ UI/Registros/Rproductos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Parcial2_MiguelGondres.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for Rproductos.xaml
+    /// </summary>
+    public partial class Rproductos : Window
+    {
+        public Rproductos()
+        {
+            InitializeComponent();
+        }
+    }
+}
